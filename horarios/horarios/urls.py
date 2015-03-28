2079c25badfa52215e9a2ef8b99e74c79f2f1c57
Diego Prado <kaoxdc@gmail.com>
2015-03-28 00:05:07 -0500

    url startic

--- horarios/horarios/urls.py
+++ horarios/horarios/urls.py
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
     url(r'^', include('horarios.apps.home.urls')),
     url(r'^', include('horarios.apps.fichas.urls')),
     url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_ROOT}),
 
     #url(r,include ('horarios.apps.fichas.urls')),
 
